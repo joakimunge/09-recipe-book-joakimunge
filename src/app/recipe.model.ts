13b0fa2dbf18c2b0ce6e91ac29c40350840d658e
Joakim Unge <ashba@unge.se>
2018-02-12 22:39:57 +0100

    Add default values to recipe model

--- src/app/recipe.model.ts
+++ src/app/recipe.model.ts
@@ -2,14 +2,34 @@ export class Recipe {
   public id: number;
   public image: string;
   public title: string;
+  public ingredients: string[];
+  public source: object;
+  public servings: number;
+  public totalTime: string;
+  public rating: number;
+
 
   constructor(
+
     id: number,
     image: string,
     title: string,
+    ingredients: string[] = null,
+    source: object = null,
+    servings: number = null,
+    totalTime: string = null,
+    rating: number = null
+
   ) {
+
     this.id = id;
     this.image = image;
     this.title = title;
+    this.ingredients = ingredients;
+    this.source = source;
+    this.servings = servings;
+    this.totalTime = totalTime;
+    this.rating = rating;
+
   }
 }
