de52d664526e301a53af6117f3b513a7c631632e
Joakim Unge <ashba@unge.se>
2018-02-14 22:08:37 +0100

    Init register and login routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,7 +8,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 	{ path: '', redirectTo: '/recipes', pathMatch: 'full'},
 	{ path: 'recipes', component: RecipesComponent },
-	{ path: 'recipe/:id', component: RecipeDetailComponent}
+	{ path: 'recipe/:id', component: RecipeDetailComponent},
+	{ path: 'login', component: LoginComponent},
+	{ path: 'register', component: RegisterComponent}
+
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { FormsModule } from '@angular/forms';
 
 import { RecipeService } from './recipe.service';
-
-import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { AppRoutingModule } from './/app-routing.module';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './shared/auth.service';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
@@ -16,15 +21,27 @@ import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
     AppComponent,
     RecipesComponent,
     RecipeDetailComponent,
-    RecipeSearchComponent
+    RecipeSearchComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return localStorage.getItem('access_token');
+        },
+        whitelistedDomains: ['dev.manchildman.com']
+      }
+    })
   ],
   providers: [
-  	RecipeService
+  	RecipeService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
