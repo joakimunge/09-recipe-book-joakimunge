2fc8b7c0359bbf1912e59dd9a201d1ce2e650e80
Joakim Unge <ashba@unge.se>
2018-02-15 23:18:42 +0100

    Add logincheck to auth service

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
@@ -21,7 +21,7 @@ export class AuthService {
   constructor(
   	private http: HttpClient,
   	private jwtHelper: JwtHelperService,
-  	private location: Location
+  	private router: Router
   ) { }
 
   login(email: string, password: string) {
@@ -38,12 +38,26 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('currentUser');
-    this.location.back(); //Temp
-    
+    this.router.navigate(['/recipes']);
   }
 
   register(email: string, password: string) {
   	return this.http.post<any>('http://dev.manchildman.com/auth/register', {email: email, password: password});
+  }
+
+  isUserLoggedin() {
+    const user = localStorage.getItem('currentUser');
+
+    if (user && !this.jwtHelper.isTokenExpired(user)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getUser() { //Why doesn't this work with CORS but the other methods do?
+    return this.http.get<any>('http://dev.manchildman.com/lists')
+      .subscribe(user => console.log(user));
   }
 
 }
