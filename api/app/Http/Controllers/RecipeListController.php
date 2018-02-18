a55826809938a6135ffe45109eec4d21e021c0f1
Joakim Unge <ashba@unge.se>
2018-02-19 00:08:51 +0100

    Implement store, save and show methods for RecipeListcontroller

--- api/app/Http/Controllers/RecipeListController.php
+++ api/app/Http/Controllers/RecipeListController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\RecipeList;
+use App\Recipe;
 use App\User;
 
 class RecipeListController extends Controller
@@ -27,21 +28,88 @@ class RecipeListController extends Controller
     }
 
     public function store(Request $request) {
-        $list = new RecipeList;
-        $user = Auth::user();
-        $list->name = $request->name;
-        $list->user_id = $user->id;
-        $list->save();
+
+        try {
+            $list = new RecipeList;
+            $user = Auth::user();
+            $list->name = $request->name;
+            $list->user_id = $user->id;
+            $list->save();
+            return response([
+                'status' => 'success',
+                'user' => Auth::user(),
+                'list' => $list
+            ]);
+        }
+
+        catch(\Exception $e) {
+            return response([
+                'status' => 'Something went wrong..',
+                'exception' => $e->getMessage()
+            ]);
+        }
+
+        catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response(['token_expired' => 'beeee'], 500);
+
+        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response(['token_invalid'  => 'beeee'], 500);
+
+        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent' => $e->getMessage()], 500);
+
+        }
+
+    }
+
+    public function save(Request $request) {
+
+        try {
+            $user = Auth::user();
+            $list = RecipeList::find($request->list);
+
+            $recipe = new Recipe;
+            $recipe->remote_id = $request->recipe;
+            $recipe->save();
+
+            $list->recipes()->attach($recipe->id);
+
+
+            return response([
+                'status' => 'success',
+                'user' => Auth::user(),
+                'list' => $list,
+                'recipe' => $recipe,
+            ]);
+
+        } 
+
+        catch(\Exception $e) {
+            return response([
+                'status' => 'Something went wrong..',
+                'exception' => $e->getMessage(),
+                'list' => $list,
+                'recipe' => $recipe
+            ]);
+        }
     }
 
     public function show(Request $request, $id) {
 
         $list = RecipeList::find($id);
+        $recipes = [];
+        foreach($list->recipes as $recipe) {
+            $recipes[] = $recipe->pivot->remote_id;
+        }
 
         return response([
             'status' => 'success',
             'user' => Auth::user(),
-            'list' => $list
+            'list' => $list,
+            'recipes' => $recipes
         ]);
     }
 }
