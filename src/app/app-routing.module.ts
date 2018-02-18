4c46e9a9cfb3accc05d7a1b235d2e1c1f0c02cc1
Joakim Unge <ashba@unge.se>
2018-02-19 00:06:35 +0100

    Update route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { ListDetailComponent } from './list-detail/list-detail.component';
-import { RouterModule, Routes } from '@angular/router';
+import { ListCreateComponent } from './list-create/list-create.component';
+import { AuthGuardService as AuthGuard } from './auth-guard/auth-guard.service';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/recipes', pathMatch: 'full'},
 	{ path: 'recipes', component: RecipesComponent },
 	{ path: 'recipe/:id', component: RecipeDetailComponent},
 	{ path: 'login', component: LoginComponent},
-	{ path: 'register', component: RegisterComponent},
-	{ path: 'lists', component: ListsComponent},
-	{ path: 'list/:id', component: ListDetailComponent},
-
-
-
-
+	{ path: 'register', component: RegisterComponent, canActivate: [AuthGuard]},
+	{ path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
+	{ path: 'list/:id', component: ListDetailComponent, canActivate: [AuthGuard]},
+	{ path: 'lists/create', component: ListCreateComponent, canActivate: [AuthGuard]},
 
 ]
 
